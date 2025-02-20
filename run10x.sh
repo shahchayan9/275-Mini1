3dd777ff86cde33672eb5127e67da3d1da3c4241
Yash Kumar <yash.arunkumar@gmail.com>
2025-02-20 12:47:12 -0800

    Added bash script to run code 10x

---
+++ run10x.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+for i in {1..10}; do
+    echo "Run #$i"
+    rm -rf * >/dev/null 2>&1
+    cmake .. >/dev/null 2>&1
+    make >/dev/null 2>&1
+    ./collision_analysis
+    echo ""
+done
